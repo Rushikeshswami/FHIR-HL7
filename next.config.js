dd06bd8e81dd630a19f8b243f609b34631ef2fd9
Rushikesh Swami <84626413+Rushikeshswami@users.noreply.github.com>
2026-06-30 14:50:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This tells Next.js to make static files for GitHub
+  // output: 'export', // This tells Next.js to make static files for GitHub
   images: {
     unoptimized: true, // This is required for static exports
   },
